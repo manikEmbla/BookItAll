42fe228e5845c60d78cf110767e318df09efab49
manik <maniken@embla.asia>
2017-03-07 07:28:56 +0530

    Binding

--- src/app/components/hotel-short-detail/hotel-short-detail.component.ts
+++ src/app/components/hotel-short-detail/hotel-short-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hotel } from '../../models/hotel';
 
 @Component({
   selector: 'hotel-short-detail',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HotelShortDetailComponent implements OnInit {
 
   constructor() { }
+
+  @Input() hotel: Hotel;
 
   ngOnInit() {
   }
